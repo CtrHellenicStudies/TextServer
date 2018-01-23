f43f56afc38d4fe2bafee4cd7c939119dc48d5cb
Luke Hollis <lukehollis@gmail.com>
2018-01-23 02:28:36 -0500

    add translation model

---
+++ src/models/translation.js
@@ -0,0 +1,43 @@
+import Sequelize from 'sequelize';
+import SequelizeSlugify from 'sequelize-slugify';
+import db from '../db';
+
+/**
+ * A translation of a work
+ */
+const Translation = db.define('translations', {
+	id: {
+		type: Sequelize.INTEGER,
+		autoIncrement: true,
+		primaryKey: true,
+	},
+	title: {
+		type: Sequelize.STRING,
+	},
+	slug: {
+		type: Sequelize.STRING,
+		unique: true,
+	},
+	description: {
+		type: Sequelize.TEXT,
+	},
+	urn: {
+		type: Sequelize.STRING,
+	},
+}, {
+	timestamps: false,
+});
+
+Translation.associate = ({ models }) => {
+	Translation.belongsTo(models.works);
+};
+
+SequelizeSlugify.slugifyModel(Translation, {
+	source: ['title'],
+	slugOptions: { lower: true },
+	column: 'slug'
+});
+
+
+
+export default Translation;
