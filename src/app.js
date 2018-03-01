f42085445184003b3e34df6e63d2524a00ec4c23
Hao Wu <haowu0802@gmail.com>
2018-03-01 11:24:47 -0800

    separate app from index to make it flexable for tests; refine tests according to separated app;

---
+++ src/app.js
@@ -0,0 +1,71 @@
+// main
+import express from 'express';
+import fs from 'fs';
+import request from 'request';
+
+// middleware
+import bodyParser from 'body-parser';
+import session from 'express-session';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+
+// db
+import * as models from '../src/models';
+import db, { dbSetup } from './db';
+
+// dotenv
+import dotenvSetup from './dotenv';
+
+// authentication
+// import authSetup from './authentication';
+
+// Health Check
+
+import healthCheck from './healthCheck';
+
+// cors
+import corsSetup from './cors';
+
+// graphQL
+import setupGraphql from './graphql';
+
+// Routes
+// import authenticationRouter from './authentication/routes';
+
+
+// environment variables setup
+dotenvSetup();
+
+const app = express();
+
+app.set('port', (process.env.PORT || 3003));
+
+app.use(logger('dev'));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: false,
+}));
+
+// db setup
+dbSetup();
+
+// CORS setup
+corsSetup(app);
+
+// GraphQl setup
+setupGraphql(app);
+
+// Routes
+// app.use('/auth', authenticationRouter);
+app.use('/', healthCheck);
+
+// App server listen
+const listen = () => {
+	app.listen(app.get('port'), () => {
+		console.info(`App is now running on http://localhost:${app.get('port')}`);
+	});
+};
+
+export default app;
+export {db, listen};
--- tests/lwcts.test.js
+++ tests/lwcts.test.js
@@ -1,15 +1,18 @@
 // test LightWrightCTS module and its interactions with LightWeightCTSServer
-// TODO use test server instance instead of having to have the server up for testing
-const tester = require('graphql-tester').tester;
+import {tester} from 'graphql-tester';
+
+import {create as createExpressWrapper} from './appWrapper';
+import app from '../src/app';
 
 describe('LightWeightCTS module', () => {
 
-	const lwctsTest = tester({
-		url: `http://localhost:${(process.env.PORT || 3003)}/graphql`, // TODO get port from app
+	const LwctsTest = tester({
+		server: createExpressWrapper(app),
+		url: '/graphql',
 		contentType: 'application/json'
 	});
 
-	describe('GetText', async () => {
+	describe('GetTextByURN', async () => {
 
 		const gqlQuery = {
 			query: `{
@@ -20,10 +23,10 @@ describe('LightWeightCTS module', () => {
 			variables: {}
 		};
 
-		it('should get the correct text', async () => {
+		it('should get the correct text for the given URN', async () => {
 			let response;
 			try {
-				response = await lwctsTest(JSON.stringify(gqlQuery));
+				response = await LwctsTest(JSON.stringify(gqlQuery));
 				expect(response.status).toBe(200);
 				expect(response.success).toBe(true);
 				expect(response.data.lightWeightCts.MapCitationPassage).toHaveProperty(['1.1']);
